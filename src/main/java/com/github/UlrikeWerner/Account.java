a63c4882550d7503b21b759b1fa9c7771e965f7b
UlrikeWerner <ulrike_werner@web.de>
2023-09-04 14:40:36 +0200

    create Account and Client

---
+++ src/main/java/com/github/UlrikeWerner/Account.java
@@ -0,0 +1,9 @@
+package com.github.UlrikeWerner;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private String bankNumber;
+    private BigDecimal balance;
+    Client client;
+}
---
+++ src/main/java/com/github/UlrikeWerner/Client.java
@@ -0,0 +1,8 @@
+package com.github.UlrikeWerner;
+
+public record Client(
+        String clientId,
+        String firstName,
+        String lastName
+) {
+}
